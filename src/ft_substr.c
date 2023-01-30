0096769be2620add99d630ccf94d8316cbeef856
Dan Palmer <dj.palmer81@gmail.com>
2023-01-30 17:51:07 +0200

    updating substr to remove strlen for optimization purposes. see file comments

--- src/ft_substr.c
+++ src/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: dpalmer <dpalmer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 10:22:48 by dpalmer           #+#    #+#             */
-/*   Updated: 2022/11/18 18:22:35 by dpalmer          ###   ########.fr       */
+/*   Updated: 2023/01/30 17:35:15 by dpalmer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,35 @@
 
 // Allocates and returns a string from a given starting point.
 
+/* Optimization update - Jan 30, 2023. Multiple calls to ft_strlen are impacting
+the performance of FdF. In the interest of efficiency, ft_strlen is no longer
+called, which may result in the over-allocation of memory. Calloc is now used
+in place of the previous malloc. In the event a malicious actor tries to access
+any indexes beyond the NULL-terminator in the original string, the new 
+substring will also produce a series of NULL-terminators.	*/
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t	s_len;
+	size_t	i;
+	size_t	j;
 	char	*sub;
 
 	if (!s || len < 0)
 		return (NULL);
-	s_len = ft_strlen(s);
-	if (len > s_len)
-		len = s_len;
-	sub = (char *)malloc(sizeof(char) * (len + 1));
+	sub = calloc(sizeof(char), (len + 1));
 	if (!sub)
 		return (NULL);
-	if (start < s_len)
+	i = 0;
+	j = 0;
+	while (s[i])
 	{
-		ft_memmove(sub, &s[start], len);
-		sub[len] = '\0';
+		if (i >= start && j < len)
+		{	
+			sub[j] = s[i];
+			j++;
+		}
+		i++;
 	}
-	else
-		sub[0] = '\0';
+	sub[j] = 0;
 	return (sub);
 }
